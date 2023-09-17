563806861111fc4738d9f888df28731f70e364b1
Martin Yanchev <myanch200@gmail.com>
2023-09-17 18:27:50 +0100

    lazy loading images by default

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,11 @@ module RailsAsync
 
     # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.action_view.image_loading = "lazy"
 
+    # set redis as cache store
+
+    config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
